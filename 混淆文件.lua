e420476bfaa9e372d1192890a721cb1b4e45662f
longshu <137012416+dkfkfkfjfkfjdj@users.noreply.github.com>
2023-08-10 19:20:08 +0800

    Add files via upload

--- 混淆文件.lua
+++ 混淆文件.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/nJJPrp2w"))()
-local Window = OrionLib:MakeWindow({Name = "操你妈的个逼的你个傻逼东西", HidePremium = false, IntroText = "傻逼东西你妈死了"})
+local Window = OrionLib:MakeWindow({Name = "操你妈的个傻逼", HidePremium = false, IntroText = "傻逼东西你妈死了"})
 
 local about = Window:MakeTab({
     Name = "傻逼东西 ",
@@ -8,13 +8,13 @@ local about = Window:MakeTab({
 })
 
 local Tab =Window:MakeTab({
-    Name = "哦 你妈了个傻逼玩意 你妈个逼养的 ",
+    Name = "你妈了个傻逼",
 	Icon = "rbxassetid://14272184100",
 	PremiumOnly = false
 })
 
 Tab:AddTextbox({
-	Name = "婊子操你血妈你妈个的傻逼",
+	Name = "婊子操你血妈你妈个的傻逼你全家死了 ",
 	Default = "",
 	TextDisappear = true,
 	Callback = function(Value)
